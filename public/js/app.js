6decf89876d8831f1deab282ab5bee0d170635f7
Kamil Balda <k.balda@foreto.com>
2021-04-21 14:24:55 +0200

    heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', e => {
     msgOne.textContent = "loading..."
     msgTwo.textContent = ""
 
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
         .then(resp => resp.json()
             .then(data => {
                 if(data.err){
