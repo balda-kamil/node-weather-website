18607ed1b6a9bbc415763ecfff70055533b914dc
Kamil Balda <k.balda@foreto.com>
2021-04-21 14:37:07 +0200

    .env issue fixed

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,10 +4,12 @@ const geocode = (address, callback) => {
 
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.MAPBOX_TOKEN}&limit=1`
 
+    console.log(url)
     request({url, json: true}, (err, {body}) => {
         if(err){
             callback('Unable to connect to location services', undefined)
         } else if(body.features.length === 0){
+            console.log(body)
             callback('Unable to find location. Try another search', undefined)
         } else {
             callback(undefined, {
